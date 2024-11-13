788c675cf3646242e255fed1c3b1e0690cbe0b6f
Vicenzzo <vicenzzo.toth@gmail.com>
2024-11-12 23:06:50 -0300

    Commit de alterção do enum

--- src/main/java/com/api/paideia/enums/KnowledgeAreaEnum.java
+++ src/main/java/com/api/paideia/enums/KnowledgeAreaEnum.java
@@ -2,7 +2,7 @@ package com.api.paideia.enums;
 
 public enum KnowledgeAreaEnum {
 
-    CIENCIAS_AGRARIAS("Ciencias Agrarias"), CIENCIAS_BIOLÓGICAS("Ciencias Biológicas"),
+    CIENCIAS_AGRARIAS("Ciencias Agrarias"), CIENCIAS_BIOLOGICAS("Ciencias Biológicas"),
     CIENCIAS_DA_SAUDE("Ciencias da Saude"),
     CIENCIAS_EXATAS_E_DA_TERRA("Ciencias Exatas e da Terra"), CIENCIAS_HUMANAS("Ciencias Humanas"),
     CIENCIAS_SOCIAIS_APLICADAS("Ciencias Sociais Aplicadas"), ENGENHARIA("Engenharia"), LINGUISTICA("Linguistica"),
